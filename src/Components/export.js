87cc82261f5267f2ad0d52b47ba5650a3bf65359
LauM727 <LauraMarcelaM.mp26@gmail.com>
2023-03-22 18:54:21 -0500

    1st commit pt2 Lau

--- src/Components/export.js
+++ src/Components/export.js
@@ -1,3 +1,4 @@
 export { default as DashboardPost } from "./dashboardPost/dashboardPost";
 export { default as SideBar } from "./sideBar/sideBar";
 export { default as FollowRecoms } from "./followRecoms/followRecoms";
+export {default as Card} from "./card/Card";
--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
+import data from "./data";
 import * as components from "./components/export";
+
+class AppContainer extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({mode: "open"})
+
+        this.lkd=[];
+        var i=0;
+        while(i<data.length){
+            this.lkd.push(data[i]);
+            i++;
+        }
+    }
+
+    connectedCallback(){
+        this.render();
+    }
+
+    render(){
+        data.forEach((lkd)=>{
+            this.shadowRoot.innerHTML += `<link rel="stylesheet" href="./src/components/card/card.css" type="text/html">
+            <card-post name="${lkd.name}" profile="${lkd.photoPerfil}" info="${lkd.info}" post="${lkd.post}" ></card-post>`
+
+        })
+    }
+}
+
+customElements.define("app-container",AppContainer);
